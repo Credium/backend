f010e207806a618187d307c0ae0b1ff5100b64a4
JunYeongJang <wnsdud3256@gmail.com>
2017-09-27 16:02:39 +0900

    test code with 표현식 삭제
    app.test_request_context().push() 사용

--- tests/test_account.py
+++ tests/test_account.py
@@ -2,38 +2,36 @@ import pytest
 from flask import url_for
 
 from app import create_app
-from app.account.forms import LoginForm
 
 
 @pytest.fixture
 def app():
     app = create_app('testing')
-    app_context = app.app_context()
-    app_context.push()
+    app.test_request_context().push()
     return app
+
 
 @pytest.fixture
 def client(app):
     client = app.test_client()
     return client
 
+
 def test_login_success_api(app, client):
-    with app.test_request_context():
-        data = {
-            "username": "guest1",
-            "password": "guest1"
-        }
-        url = url_for('account.login')
-        response = client.post(url, data=data)
-        assert response.data.decode('utf-8') == 'validate'
+    data = {
+        "username": "guest1",
+        "password": "guest1"
+    }
+    url = url_for('account.login')
+    response = client.post(url, data=data)
+    assert response.data.decode('utf-8') == 'validate'
 
 
 def test_login_fail_api(app, client):
-    with app.test_request_context():
-        data = {
-            "username": "gue",
-            "password": "password"
-        }
-        url = url_for('account.login')
-        response = client.post(url, data=data)
-        assert response.data.decode('utf-8') == 'Hi'
+    data = {
+        "username": "gue",
+        "password": "password"
+    }
+    url = url_for('account.login')
+    response = client.post(url, data=data)
+    assert response.data.decode('utf-8') == 'invalidate'
