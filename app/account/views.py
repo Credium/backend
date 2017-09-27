aa84ad9619cd0c001d50a0bb550b96f858a31cd6
JunYeongJang <wnsdud3256@gmail.com>
2017-09-27 16:17:12 +0900

    유저 api 스켈레톤
    login, logout, register, delete, update, user_info

--- app/account/views.py
+++ app/account/views.py
@@ -4,7 +4,38 @@ from .forms import LoginForm
 
 @account.route('/login', methods=["POST"])
 def login():
+    # todo 로그인
     form = LoginForm()
     if form.validate():
         return "validate"
     return "invalidate"
+
+
+@account.route('/logout', methods=["POST"])
+def logout():
+    # todo 로그아웃
+    pass
+
+
+@account.route('/register', methods=["POST"])
+def register():
+    # todo 유저 등록
+    pass
+
+
+@account.route('/delete', methods=["DELETE"])
+def delete():
+    # todo 유저 회원 탈퇴
+    pass
+
+
+@account.route('/update', methods=["PUT"])
+def update():
+    # todo 유저 정보 업데이트
+    pass
+
+
+@account.route('/user-info', methods=["GET"])
+def user_info():
+    # todo 토큰으로 유저 정보 반환
+    pass
