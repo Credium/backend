2eb29e2eb9aba73c79825151f99142716aa98da7
JunYeongJang <wnsdud3256@gmail.com>
2017-10-16 01:16:40 +0900

    Follow 모델 변경
    Association 적용, following, follower로 네이밍 변경

--- tests/account/test_models.py
+++ tests/account/test_models.py
@@ -8,16 +8,17 @@ class TestAccountModel:
     @classmethod
     @pytest.fixture(autouse=True)
     def setUp(self, db, guest1, publisher1):
+        self.db = db
         self.guest1 = guest1
         self.publisher1 = publisher1
         self.publisher2 = User(username="publisher2", password="publisher", type="publisher")
         self.publisher2.publisher_info = PublisherInfo(description="the second publisher")
 
-        self.follow1 = Follow(subject=self.guest1)
-        self.follow2 = Follow(subject=self.guest1)
+        self.follow1 = Follow(following=self.guest1)
+        self.follow2 = Follow(following=self.guest1)
 
-        self.publisher1.follower.append(self.follow1)
-        self.publisher2.follower.append(self.follow2)
+        self.publisher1.publisher_info._follower.append(self.follow1)
+        self.publisher2.publisher_info._follower.append(self.follow2)
         db.session.add(self.guest1, self.publisher1)
         db.session.add(self.publisher2)
         db.session.commit()
@@ -35,14 +36,15 @@ class TestAccountModel:
         assert self.publisher1.publisher_info.description == "the first publisher"
 
     def test_guest_following(self):
-        assert self.guest1.following[0].object.username == "publisher1"
-        assert self.guest1.following[1].object.username == "publisher2"
+        assert self.guest1.following[0].user.username == "publisher1"
+        assert self.guest1.following[1].user.username == "publisher2"
 
     def test_publisher1_follower(self):
-        assert self.publisher1.follower[0].subject.username == "guest1"
+        assert self.publisher1.publisher_info.follower[0].username == "guest1"
 
-    def test_delete_guest_publisher1_folower(self, db):
-        follow = Follow.query.filter_by(subject=self.guest1,
-                                        object=self.publisher1).first()
-        db.session.delete(follow)
-        assert not Follow.query.filter_by(object=self.publisher1).count()
+    def test_delete_guest_publisher1_folower(self):
+        print(Follow.query.all())
+        follow = Follow.query.filter_by(following=self.guest1,
+                                        follower=self.publisher1.publisher_info).first()
+        self.db.session.delete(follow)
+        assert not Follow.query.filter_by(follower=self.publisher1.publisher_info).count()
