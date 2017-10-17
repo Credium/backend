5ac0e53c91c918ba55490903806c62413c728c18
JungWinter <wintermy201@gmail.com>
2017-10-17 23:57:38 +0900

    save_image 함수 변경
    1. 디렉토리가 없을 시 recursive하게 디렉토리 생성
    2. 반환하는 path를 app/media/folder/file 에서 folder/file로 변경

--- app/helpers.py
+++ app/helpers.py
@@ -9,14 +9,18 @@ from app.account.models import User
 
 base_image = Image.new('RGB', (100, 100))
 
+
 def save_image(folder_name='', file_name='', image=None):
     image = image if image is not None else base_image
     media_path = current_app.config["MEDIA_FILE_PATH"]
     currently = time.strftime("%y%m%d%H%M%S")
     file_name = file_name + "_" + currently + ".jpg"
-    save_path = os.path.join(media_path, folder_name, file_name)
+    direcotry_path = os.path.join(media_path, folder_name)
+    if not os.path.isdir(direcotry_path):
+        os.makedirs(direcotry_path)
+    save_path = os.path.join(direcotry_path, file_name)
     image.save(save_path)
-    return save_path
+    return os.path.join(folder_name, file_name)
 
 
 class PublisherIDSchemaMixin:
